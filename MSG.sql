71e87d8f5abec538624fad43ee9776dccf55df4c
Your Name <you@example.com>
2025-01-29 11:40:38 +0100

    BDD

---
+++ MSG.sql
@@ -0,0 +1,158 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : mer. 29 jan. 2025 à 11:40
+-- Version du serveur : 10.5.23-MariaDB-0+deb11u1
+-- Version de PHP : 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `MSG`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Chat`
+--
+
+CREATE TABLE `Chat` (
+  `id` int(11) NOT NULL,
+  `idmessage` int(11) NOT NULL,
+  `idutilisateur` int(11) NOT NULL,
+  `contenu` text NOT NULL,
+  `Date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `heure` time NOT NULL,
+  `contenu` varchar(150) NOT NULL,
+  `idutilisateur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Table_ping`
+--
+
+CREATE TABLE `Table_ping` (
+  `id` int(11) NOT NULL,
+  `idmessage` int(11) NOT NULL,
+  `idutilisateur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+CREATE TABLE `utilisateurs` (
+  `idutilisateur` int(11) NOT NULL,
+  `nom` varchar(25) NOT NULL,
+  `prenom` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `Chat`
+--
+ALTER TABLE `Chat`
+  ADD KEY `idutilisateur` (`idutilisateur`),
+  ADD KEY `idmessage` (`idmessage`);
+
+--
+-- Index pour la table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `contenu` (`contenu`),
+  ADD KEY `idutilisateur` (`idutilisateur`);
+
+--
+-- Index pour la table `Table_ping`
+--
+ALTER TABLE `Table_ping`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idmessage` (`idmessage`,`idutilisateur`),
+  ADD KEY `idutilisateur` (`idutilisateur`);
+
+--
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`idutilisateur`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `Table_ping`
+--
+ALTER TABLE `Table_ping`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `idutilisateur` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `Chat`
+--
+ALTER TABLE `Chat`
+  ADD CONSTRAINT `Chat_ibfk_1` FOREIGN KEY (`idmessage`) REFERENCES `message` (`id`),
+  ADD CONSTRAINT `Chat_ibfk_2` FOREIGN KEY (`idutilisateur`) REFERENCES `utilisateurs` (`idutilisateur`);
+
+--
+-- Contraintes pour la table `Table_ping`
+--
+ALTER TABLE `Table_ping`
+  ADD CONSTRAINT `Table_ping_ibfk_1` FOREIGN KEY (`idutilisateur`) REFERENCES `utilisateurs` (`idutilisateur`),
+  ADD CONSTRAINT `Table_ping_ibfk_2` FOREIGN KEY (`idmessage`) REFERENCES `message` (`id`);
+
+--
+-- Contraintes pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD CONSTRAINT `utilisateurs_ibfk_1` FOREIGN KEY (`idutilisateur`) REFERENCES `message` (`idutilisateur`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
